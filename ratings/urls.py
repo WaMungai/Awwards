d5a772bd784d6c6f9a84810847a5127340f75204
Joyce <wamungai29@gmail.com>
2020-02-15 12:19:19 +0300

    Create URLConf for new projects.

--- ratings/urls.py
+++ ratings/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name='home'),
+    url(r'new/project$',views.new_project,name='new-project'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
