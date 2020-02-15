b3ba09c4ba25e766574460026c064a6eb0d49eda
Joyce <wamungai29@gmail.com>
2020-02-15 13:17:05 +0300

    Install and configure registration settings.

--- userproject/urls.py
+++ userproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('ratings.urls')),
     url(r'^tinymce',include('tinymce.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
 ]
