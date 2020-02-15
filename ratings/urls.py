ce3b7ebb4a90e3983128a0d00b8206793d944e15
Joyce <wamungai29@gmail.com>
2020-02-15 16:02:23 +0300

    Create the URL pattern for search _by_title.

--- ratings/urls.py
+++ ratings/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name='home'),
-    url(r'new/project$',views.new_project,name='new-project'),
+    url(r'^new/project$',views.new_project,name='new-project'),
+    url(r'^search/',views.search_title,name='search_title'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
