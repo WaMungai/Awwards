3ec3f1041437feeda7c2f18993faff021ccb5504
Joyce <wamungai29@gmail.com>
2020-02-15 15:35:00 +0300

    Write test functinalities for project class.

--- ratings/models.py
+++ ratings/models.py
@@ -32,6 +32,20 @@ class Project(models.Model):
     profile=models.ForeignKey(Profile,null=True)
     pub_date=models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.title
+    
+    def save_project(self):
+        self.save()
+        
+    def delete_project(self):
+        self.delete()
+        
+    @classmethod
+    def get_projects(cls):
+        project=cls.objects.all()
+        return project
+        
 class Rating(models.Model):
     editor=models.ForeignKey(User,on_delete=models.CASCADE)
     project=models.ForeignKey(Project, on_delete=models.CASCADE)
--- ratings/tests.py
+++ ratings/tests.py
@@ -27,3 +27,28 @@ class ProfileTestClass(TestCase):
         self.biography.save_profile()
         firstprofile=Profile.get_profile()
         self.assertTrue(firstprofile is not None)
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.newuser=User(username='clover')
+        self.newuser.save()
+        self.projects=Project(title='code',image='img.jpg',description='icodeinheels',editor=self.newuser)
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.projects,Project))
+        
+    def test_save_project(self):
+        self.projects.save_project()
+        allprojects=Project.objects.all()
+        self.assertTrue(len(allprojects)>0)
+        
+    def test_delete_projects(self):
+        self.projects.save_project()
+        self.projects.delete_project()
+        allprojects=Project.objects.all()
+        self.assertTrue(len(allprojects)==0)
+        
+    def test_get_projects(self):
+        self.projects.save_project()
+        firstproject=Project.get_projects()
+        self.assertTrue(firstproject is not None)
