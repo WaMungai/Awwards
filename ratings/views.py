75b6bebc091a387f52904477a8ee23c85c15f8ab
Joyce <wamungai29@gmail.com>
2020-02-15 16:13:59 +0300

    Create view function that will handle logic to display search results.

--- ratings/views.py
+++ ratings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import NewProjectForm,NewRatingForm,NewProfileForm
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 # Create your views here.
 
 @login_required(login_url='/accounts/login')
@@ -24,4 +24,16 @@ def new_project(request):
             form = NewProjectForm()
         return render(request,'new_project.html',{"form":form})
     
+
+@login_required(login_url='/accounts/login')
+def search_by_title(request):
+    if'title' in request.GET and request.GET["title"]:
+        search_term=request.GET.get("title")
+        searched_titles=Project.search_by_term(search_term)
+        message=f"{search_term}"
+        
+        return render(request,"search.html",{"message":message,"titles":searched_titles})
+    else:
+        message="You haven't serached for any terms"
+        return render(request,'search.html',{"message":message})
 
